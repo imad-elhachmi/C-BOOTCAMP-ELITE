8c583974474e947f917cc83dd8bc20ae8184152f
IMAD EL HACHMI <im.imadelhachmi@gmail.com>
2025-10-15 18:00:37 +0200

    Add files via upload

---
+++ BASICS/A-THE-FUNCTION-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("hello world");  //  this is statement ;
+   //this function dont take new line
+   //this is comment C doesn't read it
+   /*
+   this is multi line comment C doesn't read it
+   */
+    return 0;
+}
