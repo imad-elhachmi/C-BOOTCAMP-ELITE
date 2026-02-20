f27a653b73dd4909a6e1ee15a9547eec2983c0b2
IMAD EL HACHMI <im.imadelhachmi@gmail.com>
2026-02-21 00:43:38 +0100

    Add files via upload

---
+++ D-CONTROL-FLOW-STATEMENTS/C-LOOPS.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(void){
+//-----------------------for loop:
+    /*
+        for(initialization statement ; test expression(condition) ; update expression)
+        {
+            //peace of code
+        }
+    */
+
+//-----------------------while loop:
+    /*
+        initialization statement;
+        while(test expression(condition))
+        {
+            //peace of code
+        update expression;
+        }
+    */
+
+//-----------------------do while loop:
+     /*
+         initialization statement;
+         do
+         {
+             //peace of code
+         update expression;
+         }while(test expression(condition));
+     */
+    return (0);
+}
+
+
+
+
