fafaa2ab4829637748b09cc21db105564fab950c
IMAD EL HACHMI <im.imadelhachmi@gmail.com>
2026-02-21 00:16:31 +0100

    Add files via upload

--- D-CONTROL-FLOW-STATEMENTS/A-if...else statement.c
+++ D-CONTROL-FLOW-STATEMENTS/A-if...else statement.c
@@ -1,1 +1,64 @@
+#include <stdio.h>
+int main(void){
+//-----------------------if...else statement :
+    //if statement :
+    /*
+        if(condition 1)
+        {
+            //peace of code
+        }
+        if(condition 2)
+        {
+            //peace of code
+        }
+            .
+            .
+            .
+        if(condition n)
+        {
+            //peace of code
+        }
+
+        ​"
+        The final condition will be long,
+        as it negates all previous conditions;
+        it will check everything.
+        "
+    */
+
+    // if...else statement :
+    /*
+        if(condition 1)
+        {
+            //peace of code
+        }
+        else if(condition 2)
+        {
+            //peace of code
+        }
+        else if(condition 3)
+        {
+            //peace of code
+        }
+            .
+            .
+            .
+        else
+        {
+            //peace of code
+        }
+
+        ​"
+        For the final case, we simply use else without a condition;
+        it executes if none of the previous conditions are met.
+        If a true condition is found,
+         the others will not be checked.
+         "
+    */
+
+    return (0);
+}
+
+
+
 
